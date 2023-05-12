df168ee73bec00f3277d11c2746b92a4774ec4e3
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-05-12 14:51:50 +0300

    Add GET currentuser placeholder

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,11 @@ import express from 'express'
 import { json } from 'body-parser'
 
 const app = express()
-app.use(json)
+app.use(json())
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi there!')
+})
 
 const port = 3000
 app.listen(port, () => {
