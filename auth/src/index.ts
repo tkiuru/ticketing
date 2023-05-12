ffadb34dd559a60a832284aeb9760c1a940d614d
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-05-12 14:09:41 +0300

    First commit

---
+++ auth/src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import { json } from 'body-parser'
+
+const app = express()
+app.use(json)
+
+const port = 3000
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`)
+})
