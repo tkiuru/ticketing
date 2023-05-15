20c31eb7c758266bf163faf98f9282805f308664
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-05-15 16:53:32 +0300

    Add error handling middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -15,13 +15,12 @@ router.post('/api/users/signup', [
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-    return res.status(400).send(errors.array())
+    throw new Error('Invalid email or password')
   }
 
   const { email, password } = req.body
 
   console.log('Creating a user...')
-
   res.send({})
 })
 
