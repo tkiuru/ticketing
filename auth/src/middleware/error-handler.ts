33f24dfd7827d463958b3d12e47d555cb26285c7
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-05-17 16:28:16 +0300

    Introduce CustomError

--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import { DatabaseConnectionError } from '../errors/database-connection-error'
 import { RequestValidationError } from '../errors/request-validation-error'
+import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (
   err: Error,
@@ -8,11 +9,7 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() })
-  }
-
-  if (err instanceof DatabaseConnectionError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() })
   }
 
