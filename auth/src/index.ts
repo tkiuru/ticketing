4405f9b04d2927380a53c727ec0c79e28692341d
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-05-26 17:06:23 +0300

    Connect to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
@@ -23,7 +24,18 @@ app.use('*', async () => {
 
 app.use(errorHandler)
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`)
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to mongodb')
+  } catch (err) {
+    console.log(err)
+  }
+
+  const port = 3000
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}!`)
+  })
+}
+
+start()
