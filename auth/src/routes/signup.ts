a72800704f0c1fbcca9359d4358ce6646f129767
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-05-29 16:13:39 +0300

    Create user on signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,32 +1,40 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
 import { RequestValidationError } from '../errors/request-validation-error'
+import { User } from '../models/user'
 
 const router = express.Router()
 
-router.post('/api/users/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Email must be valid'),
-  body('password')
-    .trim()
-    .isLength({ min: 8, max: 50 })
-    .withMessage('Password must be between 8 and 50 characters')
-], async (req: Request, res: Response) => {
-  const errors = validationResult(req)
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 8, max: 50 })
+      .withMessage('Password must be between 8 and 50 characters'),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req)
 
-  if (!errors.isEmpty()) {
-    throw new RequestValidationError(errors.array())
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array())
+    }
+
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      console.log('Email in use')
+      return res.send({})
+    }
+
+    const user = User.build({ email, password })
+    await user.save()
+
+    res.status(201).send(user)
   }
-
-  const { email, password } = req.body
-
-  console.log('Creating a user...')
-
-  throw new DatabaseConnectionError()
-
-  res.send({})
-})
+)
 
 export { router as signupRouter }
