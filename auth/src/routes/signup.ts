776fdc975f6ec5d259f54295e3e99a25e535ff0f
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-06-02 14:30:45 +0300

    Add cookie with dummy signed jwt

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import { body, validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { User } from '../models/user'
 import { BadRequestError } from '../errors/bad-request-error'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
@@ -32,6 +33,16 @@ router.post(
 
     const user = User.build({ email, password })
     await user.save()
+
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'asdf'
+    )
+
+    req.session = { jwt: userJwt }
 
     res.status(201).send(user)
   }
