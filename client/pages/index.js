92f443ac8876fd1f7cbec87345a43307631a414a
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-06-20 17:37:28 +0300

    Extract building client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,15 @@
 import axios from 'axios'
+import buildClient from '../api/build-client'
 
 const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>
 }
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      { headers: req.headers }
-    )
-    return data
-  } else {
-    const { data } = await axios.get('/api/users/currentuser')
-    return data
-  }
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser')
+
+  return data
 }
 
 export default LandingPage
