0dd1721508c5a46ab68adfae811177ee58ef8b3b
Tiina Kiuru <tiina.kiuru@relexsolutions.com>
2023-06-20 17:22:32 +0300

    Server side render landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,24 @@
-const Landing = () => {
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>
 }
 
-export default Landing
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    )
+    return data
+  } else {
+    const { data } = await axios.get('/api/users/currentuser')
+    return data
+  }
+}
+
+export default LandingPage
